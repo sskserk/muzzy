3a5d8b59849cd7c165dc1f33f87d70cd438f436b
Ser Kim <serk.kmh@gmail.com>
2026-01-14 15:41:35 +0100

    html.lang

---
+++ src/app/(frontend)/posts/layout.tsx
@@ -0,0 +1,33 @@
+import type { ReactNode } from 'react'
+
+type LocaleLayoutProps = {
+  children: ReactNode
+  params: {
+    locale: string
+  }
+}
+
+export default function LocaleLayout({ children, params }: LocaleLayoutProps) {
+  return (
+    <html>
+      <head>
+        {/* <InitTheme /> */}
+        <link href="/favicon.ico" rel="icon" sizes="32x32" />
+        <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
+      </head>
+      <body>
+        {/* <Providers> */}
+        {/* <AdminBar 
+            adminBarProps={{
+              preview: isEnabled,
+            }}
+          /> */}
+
+        {/* <Header /> */}
+        {/* <Footer /> */}
+        {children}
+        {/* </Providers> */}
+      </body>
+    </html>
+  )
+}
